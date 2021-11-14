9b250e970750f0c318075da5658407dab5feb43d
JAEWOOKJUNG <stich9208@seerslab.com>
2021-11-14 22:31:29 +0900

    test2

--- week4/pages/index.tsx
+++ week4/pages/index.tsx
@@ -1,5 +1,5 @@
 import { NextPage } from 'next';
 
-const IndexPage: NextPage = () => <>Hello world test!</>;
+const IndexPage: NextPage = () => <>Hello world test2!</>;
 
 export default IndexPage;
